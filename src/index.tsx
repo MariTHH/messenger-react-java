0ee6c7a6b185fbbd4393f19a1f1859f256685ed8
tolst <tolstykh_mariia@mail.ru>
2024-07-29 15:20:40 +0300

    Co-authored-by: Daria Shevchenko <dariayo@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 
@@ -15,6 +16,8 @@ root.render(
   </BrowserRouter>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
 
